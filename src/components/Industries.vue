b09e79b2637defd47e428a38af69ef3d6ddbfb9e
akashchaubey <akashchaubey.sky@gmail.com>
2022-02-27 13:46:33 +0530

    changes industries section

--- src/components/Industries.vue
+++ src/components/Industries.vue
@@ -6,19 +6,20 @@
                 </div>
                 <div class="industries align-items-center d-flex mt-md-5">
                     <div class="industries_name mr-md-5">
-                        <div class="industries_title" @mouseover="showIndustries('food_drink')">Food & Drink</div>
-                        <!-- <div class="industries_title" @mouseover="showIndustries('education')">Education</div> -->
-                        <div class="industries_title" @mouseover="showIndustries('real_state')">Real State</div>
-                        <div class="industries_title" @mouseover="showIndustries('e_commerce')">E-commerce</div>
-                        <div class="industries_title" @mouseover="showIndustries('health_fitness')">Sports & Fitness</div>
-                        <!-- <div class="industries_title" @mouseover="showIndustries('entertainment')">Entertainment</div> -->
-                        <div class="industries_title" @mouseover="showIndustries('travel_tourism')">Travel & Tourism</div>
-                        <!-- <div class="industries_title" @mouseover="showIndustries('sports')">Sports</div> -->
+                        <div class="industries_title" @click="showIndustries('e_commerce')">E-commerce</div>
+                        <div class="industries_title" @click="showIndustries('food_drink')">Food & Drink</div>
+                        <div class="industries_title" @click="showIndustries('real_state')">Real State</div>
+                        <div class="industries_title" @click="showIndustries('health_fitness')">Sports & Fitness</div>
+                        <div class="industries_title" @click="showIndustries('travel_tourism')">Travel & Tourism</div>
                     </div>
                     <div class="industries_img d-none d-md-block">
-                        <div class="image_slide" v-for="d in data" :key="d.id">
-                            <img class="img1" v-if="type == d.type" :src="d.img1" :alt="d.type" data-aos="fade-right" data-aos-delay="50">
-                            <img class="img2" v-if="type == d.type" :src="d.img2" :alt="d.type" data-aos="fade-left" data-aos-delay="50">
+                        <div class="image_slide" v-for="d in data" :key="d.id" :class="d.className">
+                            <div class="img-1">
+                                <img v-if="type == d.type" :src="d.img1" :alt="d.type" data-aos="fade-right" data-aos-delay="50">
+                            </div>
+                            <div class="img2">
+                                <img v-if="type == d.type" :src="d.img2" :alt="d.type" data-aos="fade-left" data-aos-delay="50">
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -29,56 +30,44 @@
 export default {
     data(){
         return {
-            type:'food_drink',
+            type:'e_commerce',
             data:[
                 {
                     id:1,
-                    type:'food_drink',
-                    img1:'/img/industries/food-drinks.png',
-                    img2:'/img/industries/mobile-view.png',
+                    type:'e_commerce',
+                    img1:'/img/industries/ecommerce-1.png',
+                    img2:'/img/industries/ecommerce-2.png',
+                    className:'e-commerce',
                 },
                 {
                     id:2,
-                    type:'education',
-                    img1:'/img/industries/2.jpg',
-                    img2:'/img/industries/phone-2.png',
+                    type:'food_drink',
+                    img1:'/img/industries/food-drinks.png',
+                    img2:'/img/industries/mobile-view.png',
+                    className:'food-drink',
                 },
                 {
                     id:3,
                     type:'real_state',
                     img1:'/img/industries/real-estate-1.png',
                     img2:'/img/industries/real-estate-2.png',
+                    className:'real-state',
                 },
+                
                 {
                     id:4,
-                    type:'e_commerce',
-                    img1:'/img/industries/ecommerce-1.png',
-                    img2:'/img/industries/ecommerce-2.png',
-                },
-                {
-                    id:5,
                     type:'health_fitness',
                     img1:'/img/industries/sport-fitness-1.png',
                     img2:'/img/industries/sport-fitness-2.png',
+                    className:'health-fitness',
                 },
                 {
-                    id:6,
-                    type:'entertainment',
-                    img1:'/img/industries/6.jpg',
-                    img2:'/img/industries/phone-6.png',
-                },
-                {
-                    id:7,
+                    id:5,
                     type:'travel_tourism',
-                    img1:'/img/industries/travel-tourism-1.png',
+                    img1:'/img/industries/tour-travel-1.png',
                     // img2:'/img/industries/phone-7.png',
-                },
-                {
-                    id:8,
-                    type:'sports',
-                    img1:'/img/industries/8.png',
-                    img2:'/img/industries/phone-8.png',
-                },
+                    className:'travel-tourism',
+                }
             ]
         }
     },
@@ -92,7 +81,7 @@ export default {
 <style lang="scss">
 .industries {
     .industries_name {
-        width: 250px;
+        width: 280px;
         .industries_title {
             font-size: 2.1vw;
             line-height: 2.5vw;
@@ -100,27 +89,53 @@ export default {
             cursor: pointer;
             margin: 0px 0 15px;
             white-space: nowrap;
-             color: var(--font-stl-one);
+            color: var(--font-stl-one);
         }
     }
     .industries_img {
-        width: calc(100% - 250px);
+        width: calc(100% - 280px);
         // position: relative;
         .image_slide {
             position: relative;
-            img {
+            img{
                 width: 100%;
             }
-            // .img1 {
-            //     max-height: 500px;
-            // }
-            .img2 {
+            .img2{
                 position: absolute;
-                bottom: 0;
-                right: 20%;
-                // height: 55vh;
-                max-width: 270px;
-                height: auto;
+            }
+            &.food-drink{
+                .img2{
+                    right: 20%;
+                    width: 28%;
+                    bottom: 10%;
+                }
+            }
+            &.real-state{
+                .img2{
+                    left: 20%;
+                    width: 23%;
+                    bottom: 10%;
+                    transform: rotate(32deg);
+                }
+            }
+            &.e-commerce{
+                .img2{
+                    right: 10%;
+                    width: 34%;
+                    bottom: 4%;
+                }
+            }
+            &.health-fitness{
+                .img2{
+                    right: 8%;
+                    width: 21%;
+                    bottom: 14%;
+                }
+            }
+            &.travel-tourism{
+                .img2{
+                    display: none;
+                }
             }
         }
     }
