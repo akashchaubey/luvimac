8a324be569ced5e141ef374d51bf0d483404f1f2
akash <akash@krishaweb.net>
2022-04-05 00:10:59 +0530

    web development changes

--- src/views/Web-development.vue
+++ src/views/Web-development.vue
@@ -1,7 +1,31 @@
 <template>
     <div class="inner">
     <Pagetitle/>
-    <Texttitle />
+    <section class="lm-services bg section">
+      <div class="container wd-container">
+        <div class="sec-title text-center" data-aos="fade-down" data-aos-delay="50">
+          <h2>What We Do.</h2>
+          <p>We’re best placed to help build your online business: taking the time to learn about your company,<br> industry and commercial challenges</p>
+        </div>
+        <div class="row flex-md-row-reverse">
+            <div class="col-lg-6 col-md-6 col-12">
+                <img src="/img/web-development/web-development.png" data-aos="fade-left" data-aos-delay="300" class="img-fluid">
+            </div>
+            <div class="col-lg-6 col-md-6 col-12 d-flex align-items-center">
+              <div class="ctn-box" data-aos="fade-right" data-aos-delay="80">
+                <div class="sec-title">
+                  <h2>Tools and Technologies</h2>
+                </div>
+                <!-- <div class="sub-title">
+                  <h3>Full-service IT support</h3>
+                </div> -->
+                <p>At Luvimac We provide our managed IT services to small and big size of businesses that have had software developed and to organizations that are looking for outsourcing expertise for the maintenance and development of their application. Whatever form of IT resource you are looking for, we can support you in this.</p>
+                <!-- <a href="#" class="btn stl-one">Learn More</a> -->
+              </div>
+            </div>
+          </div>
+      </div>
+    </section>
     <Doweworks :commonServices="services" :servicesSectionClass="'web'"></Doweworks>
     <!-- <Services :commonServices="services" :servicesSectionClass="'web'"></Services> -->
     <section class="how-it-work section" :style="{'background-image': `url('${howItWork.bgImg} ')`}">
